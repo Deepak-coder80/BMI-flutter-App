aa133a973c0de94b44f5daf273d7e5388d4b218d
Deepak M S <souparnikad40@gmail.com>
2021-09-18 19:44:50 +0530

    previous commit (forget to add file)

---
+++ lib/util/icon_content.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class cardchildWidget extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  const cardchildWidget({
+    Key? key,
+    required this.icon,
+    required this.label,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          color: Colors.white,
+          size: 80,
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+        Text(
+          label,
+          style: const TextStyle(
+            fontSize: 18,
+            color: Color(
+              0xFF8D8E98,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
